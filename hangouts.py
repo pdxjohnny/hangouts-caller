bda610427ed8d6c08bf4d7221c1d6493c3acefac
John Andersen <john.s.andersen@intel.com>
2016-02-01 07:58:15 -0800

    Moved images to their own directory
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- hangouts.py
+++ hangouts.py
@@ -9,6 +9,11 @@ import subprocess
 import states
 
 TYPE_INTERVAL = 0.05
+DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGE_DIR = os.path.join(DIR, 'images')
+
+def image(name):
+    return os.path.join(IMAGE_DIR, name)
 
 def notify_browser_started(connection):
     '''
@@ -46,20 +51,8 @@ def start_browser(connection):
     # Return a handle on the browser process
     return process
 
-def navigate(url):
-    # Go to google hangouts
-    location = pyautogui.locateOnScreen('firefox-search.png')
-    if location is None:
-        assert False, 'Could not find address bar'
-    pyautogui.moveTo(location[0] - (location[2] * 2), location[1] + (location[3] / 2))
-    pyautogui.click()
-    time.sleep(1)
-    pyautogui.typewrite('https://hangouts.google.com/', interval=TYPE_INTERVAL)
-    pyautogui.press('enter')
-    time.sleep(2)
-
 def allow_plugin_always():
-    location = pyautogui.locateOnScreen('allow-plugin.png')
+    location = pyautogui.locateOnScreen(image('allow-plugin.png'))
     if location is None:
         print'Could not find plugin button'
         return
@@ -72,15 +65,6 @@ def allow_plugin_always():
     pyautogui.click()
     time.sleep(2)
 
-def click_sign_in():
-    # Sign in
-    location = pyautogui.locateOnScreen('google-sign-in.png')
-    if location is None:
-        print'Could not find sign in'
-        return
-    pyautogui.moveTo(location[0] + (location[2] / 2), location[1] + (location[3] / 2))
-    pyautogui.click()
-
 def sign_in(email, password):
     pyautogui.typewrite(email, interval=TYPE_INTERVAL)
     pyautogui.press('enter')
@@ -90,22 +74,11 @@ def sign_in(email, password):
     time.sleep(3)
 
 def callsetup():
-    location = pyautogui.locateOnScreen('hangouts-in-address-bar.png')
-    if location is None:
-        print'Could not find hangouts.google.com in the address bar'
-        return
-    location_x, location_y = pyautogui.center(location)
-    pyautogui.moveTo(location_x, location_y)
-    time.sleep(0.5)
-    # Click to remove the remember password dialoge
-    pyautogui.moveRel(0, 350)
-    pyautogui.click()
-    time.sleep(1)
     # Hangouts rotates through a few lines of text in its search bar
     # any of them coudl be displayed so check them all and click if found
     search_bar_options = [
-        'hangouts-search.png', \
-        'hangouts-search-2.png'
+        image('hangouts-search.png'), \
+        image('hangouts-search-2.png')
     ]
     for option in search_bar_options:
         location = pyautogui.locateOnScreen(option)
@@ -121,7 +94,7 @@ def callnumber(number):
     pyautogui.typewrite(number, interval=TYPE_INTERVAL) # type with quarter-second pause in between each key
     # pyautogui.press('enter')
     time.sleep(1)
-    location = pyautogui.locateOnScreen('call-button.png')
+    location = pyautogui.locateOnScreen(image('call-button.png'))
     if location is None:
         assert False, 'Could not find call button'
         return
@@ -130,7 +103,7 @@ def callnumber(number):
     time.sleep(1)
 
 def hangup():
-    location = pyautogui.locateOnScreen('hangup.png')
+    location = pyautogui.locateOnScreen(image('hangup.png'))
     if location is None:
         assert False, 'Could not find hangup button'
         return
@@ -143,5 +116,7 @@ def main():
     time.sleep(10)
     allow_plugin_always()
     time.sleep(5)
-    callsetup()
     callnumber(sys.argv[3])
+
+if __name__ == '__main__':
+    main()
